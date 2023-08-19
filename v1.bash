edd922e39ab0bcd48f6ceffa368f445d268ed6dc
jay-sus <jayysus@proton.me>
2023-08-19 14:00:40 +0300

    More testing

--- v1.bash
+++ v1.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-( # Output everything to v1.log
+( # Output everything to comfy.log
 
 # =============== Configuration ===============
 
@@ -7,30 +7,42 @@ target="/dev/sda"
 ucode="intel-ucode"
 kernel="linux"
 editor="nano"
-rootmnt="/mnt"
 locale="en_US.UTF-8"
 keymap="us"
 timezone="Europe/London"
 reflector="UK,US"
-hostname="arch"
-username="test"
+hostname="comfy"
+username=""
 
-
-### Packages to pacstrap ##
-pacstrappacs=(
+essential=(
     $kernel
     $ucode
     $editor
     linux-firmware
     base-devel
     networkmanager
-)    
-### Desktop packages #####
-guipacs=(
-	neofetch
 )
 
+# =============== Pre-run checks ===============
 
+echo "[comfy] Checking root..."
+if [[ "$UID" -ne 0 ]]; then
+    echo "[comfy] This script needs to be run as root!" >&2
+    exit 3
+fi
+
+echo "[comfy] Checking configuration..."
+if [[ -z "$username" ]]; then
+    echo "[comfy] Configure this script before running!" >&2
+    exit 3
+fi
+
+echo "[comfy] Checking internet..."
+ping -c 1 "1.1.1.1" &> /dev/null
+if [[ $? -ne 0 ]]; then
+    echo "[comfy] Connect to the internet!" >&2
+    exit 3
+fi
 
 
 # Partition
@@ -53,43 +65,48 @@ mkfs.vfat -F32 -n EFISYSTEM /dev/disk/by-partlabel/EFISYSTEM
 mkfs.ext4 -L linux /dev/mapper/root
 # mount the root, and create + mount the EFI directory
 echo "Mounting File Systems..."
-mount /dev/mapper/root "$rootmnt"
-mkdir "$rootmnt"/efi -p
-mount -t vfat /dev/disk/by-partlabel/EFISYSTEM "$rootmnt"/efi
+mount /dev/mapper/root /mnt
+mkdir /mnt/efi -p
+mount -t vfat /dev/disk/by-partlabel/EFISYSTEM /mnt/efi
 
 
-
-#Update pacman mirrors and then pacstrap base install
-echo "Pacstrapping..."
+echo "[comfy] Updating pacman mirrorlist..."
 reflector --country $reflector --age 24 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-pacstrap -K $rootmnt "${pacstrappacs[@]}" 
+
+echo "[comfy] Installing base package..."
+pacstrap -K /mnt
+
+echo "[comfy] Installing essential packages..."
+arch-chroot /mnt pacman -Sy "${essential[@]}" --noconfirm --quiet
+
+
 
 echo "Setting up environment..."
 #set up locale/env
 #add our locale to locale.gen
-sed -i -e "/^#"$locale"/s/^#//" "$rootmnt"/etc/locale.gen
+sed -i -e "/^#"$locale"/s/^#//" /mnt/etc/locale.gen
 #remove any existing config files that may have been pacstrapped, systemd-firstboot will then regenerate them
-rm "$rootmnt"/etc/{machine-id,localtime,hostname,shadow,locale.conf} ||
-systemd-firstboot --root "$rootmnt" \
+rm /mnt/etc/{machine-id,localtime,hostname,shadow,locale.conf} ||
+systemd-firstboot --root /mnt \
 	--keymap="$keymap" --locale="$locale" \
 	--locale-messages="$locale" --timezone="$timezone" \
 	--hostname="$hostname" --setup-machine-id \
 	--welcome=false
-arch-chroot "$rootmnt" locale-gen
+arch-chroot /mnt locale-gen
 echo "Configuring for first boot..."
 #add the local user
-arch-chroot "$rootmnt" useradd -G wheel -m "$username"
+arch-chroot /mnt useradd -G wheel -m "$username"
 arch-chroot /mnt passwd "$username"
 
 #uncomment the wheel group in the sudoers file
-sed -i -e '/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/s/^# //' "$rootmnt"/etc/sudoers
+sed -i -e '/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/s/^# //' /mnt/etc/sudoers
 #create a basic kernel cmdline, we're using DPS so we don't need to have anything here really, but if the file doesn't exist, mkinitcpio will complain
-echo "quiet rw" > "$rootmnt"/etc/kernel/cmdline
+echo "quiet rw" > /mnt/etc/kernel/cmdline
 #change the HOOKS in mkinitcpio.conf to use systemd hooks
 sed -i \
     -e 's/base udev/base systemd/g' \
     -e 's/keymap consolefont/sd-vconsole sd-encrypt/g' \
-    "$rootmnt"/etc/mkinitcpio.conf
+    /mnt/etc/mkinitcpio.conf
 #change the preset file to generate a Unified Kernel Image instead of an initram disk + kernel
 sed -i \
     -e '/^#ALL_config/s/^#//' \
@@ -97,38 +114,36 @@ sed -i \
     -e '/^#default_options/s/^#//' \
     -e 's/default_image=/#default_image=/g' \
     -e "s/PRESETS=('default' 'fallback')/PRESETS=('default')/g" \
-    "$rootmnt"/etc/mkinitcpio.d/linux.preset
+    /mnt/etc/mkinitcpio.d/linux.preset
 
 #read the UKI setting and create the folder structure otherwise mkinitcpio will crash
-declare $(grep default_uki "$rootmnt"/etc/mkinitcpio.d/linux.preset)
-arch-chroot "$rootmnt" mkdir -p "$(dirname "${default_uki//\"}")"
+declare $(grep default_uki /mnt/etc/mkinitcpio.d/linux.preset)
+arch-chroot /mnt mkdir -p "$(dirname "${default_uki//\"}")"
 
-#install the gui packages
-echo "Installing GUI..."
-arch-chroot "$rootmnt" pacman -Sy "${guipacs[@]}" --noconfirm --quiet
+
 
 
 #enable the services we will need on start up
 echo "Enabling services..."
-systemctl --root "$rootmnt" enable systemd-resolved systemd-timesyncd NetworkManager sddm
+systemctl --root /mnt enable systemd-resolved systemd-timesyncd NetworkManager sddm
 #mask systemd-networkd as we will use NetworkManager instead
-systemctl --root "$rootmnt" mask systemd-networkd
+systemctl --root /mnt mask systemd-networkd
 #regenerate the ramdisk, this will create our UKI
 echo "Generating UKI and installing Boot Loader..."
-arch-chroot "$rootmnt" mkinitcpio -p linux
+arch-chroot /mnt mkinitcpio -p linux
 echo "Setting up Secure Boot..."
 if [[ "$(efivar -d --name 8be4df61-93ca-11d2-aa0d-00e098032b8c-SetupMode)" -eq 1 ]]; then
-arch-chroot "$rootmnt" sbctl create-keys
-arch-chroot "$rootmnt" sbctl enroll-keys -m
-arch-chroot "$rootmnt" sbctl sign -s -o /usr/lib/systemd/boot/efi/systemd-bootx64.efi.signed /usr/lib/systemd/boot/efi/systemd-bootx64.efi
-arch-chroot "$rootmnt" sbctl sign -s "${default_uki//\"}"
+arch-chroot /mnt sbctl create-keys
+arch-chroot /mnt sbctl enroll-keys -m
+arch-chroot /mnt sbctl sign -s -o /usr/lib/systemd/boot/efi/systemd-bootx64.efi.signed /usr/lib/systemd/boot/efi/systemd-bootx64.efi
+arch-chroot /mnt sbctl sign -s "${default_uki//\"}"
 else
 echo "Not in Secure Boot setup mode. Skipping..."
 fi
 #install the systemd-boot bootloader
-arch-chroot "$rootmnt" bootctl install --esp-path=/efi
+arch-chroot /mnt bootctl install --esp-path=/efi
 #lock the root account
-arch-chroot "$rootmnt" usermod -L root
+arch-chroot /mnt usermod -L root
 #and we're done
 
 
